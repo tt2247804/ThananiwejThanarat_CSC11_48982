8db6d8b9481987791895218be190e1fbf2dffb4e
Thanarat Thananiwej <thanarat_pay@hotmail.com>
2015-12-14 14:33:29 -0800

    tt2247804

--- Proj/Proj2/project2.s
+++ Proj/Proj2/project2.s
@@ -1,5 +1,8 @@
 .data
 
+f8: .float 0.0
+f9: .float 1.0
+f10: .float 5.0
 m1: .asciz "Tic-Tac-Toe\n"
 m2: .asciz " ___ ___ ___\n"
 m3: .asciz "| %c | %c | %c |\n"
@@ -17,11 +20,11 @@ m14: .asciz "It is a draw!\n\n"
 m15: .asciz "Best of 5\n"
 m16: .asciz "Round %d\n"
 m17: .asciz "Score Board\n"
-m18: .asciz "------------\n"
+m18: .asciz "----------------------\n"
 m19: .asciz "Round %d: Draw\n"
 m20: .asciz "Round %d: Player %d wins\n"
-m21: .asciz "Win Rate\n"
-m22: .asciz "------------\n"
+m21: .asciz "\nWin Rate\n"
+m22: .asciz "----------------------\n"
 m23: .asciz "Player 1 wins at the rate of %f\n"
 m24: .asciz "Player 2 wins at the rate of %f\n"
 m25: .asciz "Both players are even.\n"
@@ -384,9 +387,11 @@ draw1:
 	mov r11, #98
 	bal drawXOBoard
 check1:
-	@mov r2, #3
-        @ldr r2, ad_str3
-        @ldr r2, [r2]
+        ldr r3, ad_str3
+        ldr r2, [r3]
+
+	mov r2, #3
+	str r2, [r3]
 
 	ldr r0, ad_m14
         bl printf
@@ -420,6 +425,16 @@ check:
 	ldr r1, [r1]
         bl printf
 end:
+	ldr r2, ad_str3
+	ldr r2, [r2]
+	ldr r3, ad_a
+	ldr r1, ad_str5
+	ldr r1, [r1]
+	mov r11, #4
+	mul r1, r1, r11
+	str r2, [r3, r1]
+
+
 	ldr r1, ad_str5
 	ldr r11, [r1]
 	add r11, r11, #1
@@ -427,10 +442,101 @@ end:
 	bal round
 
 exit:
+	ldr r0, ad_m17
+        bl printf
+
+	ldr r0, ad_m18
+        bl printf
+
+	mov r4, #0
+	ldr r8, ad_f8
+	vldr s8, [r8]
+	vldr s9, [r8]
+	ldr r9, ad_f9
+	vldr s10, [r9]
+	ldr r10, ad_f10
+	vldr s11, [r10]
+	mov r8, #0
+	mov r9, #0
+
+table:
+	mov r7, r4
+	add r7, r7, #1
+	mov r5, r4
+	mov r6, #4
+	mul r5, r5, r6
+
+	ldr r3, ad_a
+	ldr r3, [r3, r5]
+
+	cmp r3, #3
+	bne tableWin
+
+	ldr r0, ad_m19
+	mov r1, r7
+        bl printf
+	bal exit1
+
+tableWin:
+	mov r1, r7
+	mov r2, r3
+
+	cmp r2, #1
+	bne win2
+	add r8, r8, #1
+	vadd.f32 s8, s8, s10
+	bal printWin
+win2:
+	add r9, r9, #1
+	vadd.f32 s9, s9, s10
+printWin:
+	ldr r0, ad_m20
+        bl printf
+exit1:
+	add r4, r4, #1
+	cmp r4, #5
+	blt table
+
+	ldr r0, ad_m21
+        bl printf
+
+	ldr r0, ad_m22
+        bl printf
+
+	cmp r8, r9
+	bgt winRate1
+
+	cmp r8, r9
+	blt winRate2
+
+	bal winRate3
+
+winRate1:
+	vdiv.f32 s12, s8, s11
+	vcvt.f64.f32 d4, s12
+	ldr r0, ad_m23
+	vmov r2, r3, d4
+        bl printf
+	bal exitGame
+winRate2:
+	vdiv.f32 s13, s9, s11
+        vcvt.f64.f32 d4, s13
+        ldr r0, ad_m24
+        vmov r2, r3, d4
+        bl printf
+	bal exitGame
+winRate3:
+	ldr r0, ad_m25
+	bl printf
+exitGame:
         ldr lr, ad_return
         ldr lr, [lr]
         bx lr
 
+ad_f8: .word f8
+ad_f9: .word f9
+ad_f10: .word f10
+ad_a: .word a
 ad_m1: .word m1
 ad_m9: .word m9
 ad_m10: .word m10
@@ -440,6 +546,15 @@ ad_m13: .word m13
 ad_m14: .word m14
 ad_m15: .word m15
 ad_m16: .word m16
+ad_m17: .word m17
+ad_m18: .word m18
+ad_m19: .word m19
+ad_m20: .word m20
+ad_m21: .word m21
+ad_m22: .word m22
+ad_m23: .word m23
+ad_m24: .word m24
+ad_m25: .word m25
 ad_scan: .word scan
 ad_scan1: .word scan1
 ad_read: .word read
